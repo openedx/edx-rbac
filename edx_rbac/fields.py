4cd8c409931dd23bcefc746a25cd8a909ec928a7
Christopher Pappas <cpappas@edx.org>
2019-03-18 13:06:33 -0400

    oops. forgot to change the other reference to User

--- edx_rbac/fields.py
+++ edx_rbac/fields.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import, unicode_literals
 from django import forms
 from django.apps import apps
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 USER_MODEL = apps.get_model(settings.AUTH_USER_MODEL)
@@ -26,7 +25,7 @@ class UserFromEmailField(forms.EmailField):
         """
         try:
             user = USER_MODEL.objects.get(email=value)
-        except User.DoesNotExist:
+        except USER_MODEL.DoesNotExist:
             raise ValidationError('User with email {} does not exist'.format(value))
 
         return user
