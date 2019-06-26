f8e4516c97affde7ac48a920982fbd7d4ed3e3e0
muhammad-ammar <mammar@gmail.com>
2019-06-26 19:48:29 +0500

    pass request obj to predicate

--- edx_rbac/decorators.py
+++ edx_rbac/decorators.py
@@ -22,8 +22,10 @@ def permission_required(*permissions, **decorator_kwargs):
             else:
                 obj = fn
 
+            predicate_args = request, obj
+
             missing_permissions = [perm for perm in permissions
-                                   if not request.user.has_perm(perm, obj)]
+                                   if not request.user.has_perm(perm, predicate_args)]
             if any(missing_permissions):
                 # raises a permission denied exception causing a 403 response
                 self.permission_denied(
