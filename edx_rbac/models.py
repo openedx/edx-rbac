5964092575e16716d05806284273c9b6e83a8fd7
Chris Pappas <cpappas@edx.org>
2021-11-01 12:33:55 -0400

    chore: Resolve a RemovedInDjango40Warning

--- edx_rbac/models.py
+++ edx_rbac/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.core.exceptions import FieldDoesNotExist
 from django.db import models
 from django.db.models.base import ModelBase
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
 
