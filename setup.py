4c696b41cfa6105364a51622fbee7ea37e8a24d3
Rebecca Graber <rgraber@edx.org>
2021-11-09 13:05:19 -0500

    fix: rm extraneous parens

--- setup.py
+++ setup.py
@@ -88,7 +88,6 @@ def is_requirement(line):
     # UPDATED VIA SEMGREP - if you need to remove/modify this method remove this line and add a comment specifying why
 
     return line and line.strip() and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
-    )
 
 
 VERSION = get_version('edx_rbac', '__init__.py')
