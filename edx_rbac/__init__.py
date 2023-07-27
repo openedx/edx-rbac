f7418688b0f7f33e23980766fff313de438d4fc0
Zubair Shakoor <57657330+zubairshakoorarbisoft@users.noreply.github.com>
2023-07-27 19:14:51 +0500

    fix: Added support for django 42 (#245)
    * fix: Added support for django 42
    
    * fix: trailing line removed

--- edx_rbac/__init__.py
+++ edx_rbac/__init__.py
@@ -2,6 +2,4 @@
 Library to help managing role based access controls for django apps.
 """
 
-__version__ = '1.7.0'
-
-default_app_config = 'edx_rbac.apps.EdxRbacConfig'  # pylint: disable=invalid-name
+__version__ = '1.8.0'
--- edx_rbac/urls.py
+++ edx_rbac/urls.py
@@ -2,9 +2,9 @@
 URLs for edx_rbac.
 """
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="edx_rbac/base.html")),
+    re_path(r'', TemplateView.as_view(template_name="edx_rbac/base.html")),
 ]
